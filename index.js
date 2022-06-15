1dc1fdb614a4a4944aa43191ab3b81215b8f7840
arun-esh <nysdas@icloud.com>
2022-06-15 17:35:42 -0400

    Added: Homepage and About Page
    Using express, sending get request for '/' which in turn will send the requested page '/'.
    For 'about', it will successfully send the about as response.

--- index.js
+++ index.js
@@ -1,21 +1,40 @@
 const express = require(`express`);
-const app = express();          // execute the express
+const app = express(); // execute the express
 
-app.get('', (req, res)=>{
-    res.send(`
-    <h1>Welcome to Home Page</h1>
+app.get('', (req, res) => {
+  res.send(`
+    <h1 style=color:blue;text-align:center;>Welcome to Home Page</h1>
     <a href="/about"> Go to about page</a>
-    `);
 
+    <br>
+
+   <h1>lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
+   <h2>lorem ipsum dolor sit amet consectetur adipisicing elit.</h2>
+   <h3>lorem ipsum dolor sit amet consectetur adipisicing elit.</h3>
+   <h4>lorem ipsum dolor sit amet consectetur adipisicing elit.</h4>
+   <h5>lorem ipsum dolor sit amet consectetur adipisicing elit.</h5>
+   <h6>lorem ipsum dolor sit amet consectetur adipisicing elit.</h6>
+
+    `);
 });
 
-app.get('/about', (req, res)=>{
-    res.send(`
-    <input type="text" placeholder="user name" />
+app.get('/about', (req, res) => {
+  res.send(`
+    <h1 style=color:blue;text-align:center;>About Page</h1>
     <br>
-    <button> Click me </button>
+
     <br>
-    <a href="/"> Go to Home page</a>
+    <a style=color:blue; href="/"> Go to home page</a>
+    <br>
+
+   <h1>lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
+   <h2>lorem ipsum dolor sit amet consectetur adipisicing elit.</h2>
+   <h3>lorem ipsum dolor sit amet consectetur adipisicing elit.</h3>
+   <h4>lorem ipsum dolor sit amet consectetur adipisicing elit.</h4>
+   <h5>lorem ipsum dolor sit amet consectetur adipisicing elit.</h5>
+   <h6>lorem ipsum dolor sit amet consectetur adipisicing elit.</h6>
+   
+    
     `);
 });
 
